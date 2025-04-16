aa0eb3504240a6e4911eb28d1e761f5c6970bfb7
CavaniAssassinn <nathanantha14@gmail.com>
2025-04-16 12:44:13 +0200

    Added Walking Animation

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    private bool isMoving;
+    private Vector2 input;
+
+    Animator animator;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            //remove diagonal movement
+            if (input.x != 0) 
+            {
+                input.y = 0;
+
+            }
+
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);    
+                animator.SetFloat("moveY", input.y);    
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
