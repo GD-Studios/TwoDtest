e6683d78c4129d9fb5737a086347f987aae20354
CavaniAssassinn <nathanantha14@gmail.com>
2025-04-20 02:51:38 +0200

    Added Moves

--- Assets/Scripts/Pokemons/PokemonBase.cs
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new Pokemon")]
@@ -21,6 +23,8 @@ public class PokemonBase : ScriptableObject
     [SerializeField] int spDefense;
     [SerializeField] int speed;
 
+    [SerializeField] List<LearnableMove> learnableMoves; 
+
     public string Name{ get{ return name; }}
     public string Description{ get{ return description; } }
 
@@ -43,6 +47,20 @@ public class PokemonBase : ScriptableObject
     public int SpDefense { get{return spDefense; } }    
 
     public int Speed { get{ return speed; } }  
+
+    public List<LearnableMove> LearnableMoves { get { return learnableMoves; } }
+
+}
+
+[System.Serializable]
+public class LearnableMove
+{
+    [SerializeField] MoveBase moveBase;
+    [SerializeField] int level;
+
+    public MoveBase Base {  get{ return moveBase; } }
+
+    public int Level { get{ return level; } }
 
 }
 public enum PokemonType 
